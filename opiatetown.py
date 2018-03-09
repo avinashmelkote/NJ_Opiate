2671b83a10f60a6decb7a8baaa2066e65c9b6b4d
avinashmelkote <avinashmelkote@gmail.com>
2018-03-09 13:05:50 -0500

    Update opiatetown.py

--- opiatetown.py
+++ opiatetown.py
@@ -1,6 +1,7 @@
 #Identify the towns with the highest number of opiate-related overdoses in New Jersey between 2004 and 2014.
 import re 
 opiate = raw_input("Enter File Name: ")
+if len(opiate) < 1 : opiate = "njopiate.txt"
 hand = open(opiate)
 d = dict()
 for line in hand:
@@ -17,6 +18,10 @@ for key, val in d.items():
 
 lst.sort(reverse=True)
 
-print "50 towns with the most opiate overdoses between 2004 and 2014: ", lst[:50]
-#print "50 towns with the opiate overdoses: ", lst[-50:]
-print "Number of towns with opiate overdoses from 2004 to 2014: ", len(lst)
+#print "50 towns with the most opiate overdoses: ", lst[:50]
+#print "50 towns with the least opiate overdoses: ", lst[-50:]
+print "Number of towns with opiate-related overdoses from 2004 to 2014: ", len(lst)
+
+print "List of 50 towns with the most opiate overdoses: "
+for apple, orange in lst[:50] :
+	print orange, apple
